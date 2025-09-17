a8ee47f348a3592b76d0915c911069536f0ae933
Ana Lessa <analessaferreira.aa@gmail.com>
2025-09-17 19:42:35 +0000

    PRATICANDO

---
+++ aula_01_teorica/Exer01.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Exer01 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite a quantidade: ");
+        int quantidade = entrada.nextInt();
+        System.out.println("Digite o preco do produto: ");
+        float preco = entrada.nextFloat();
+
+        float total = quantidade*preco;
+
+        System.out.println("\n Nota Fiscal");
+        System.out.println("\n Quantid: " +quantidade);
+        System.out.println("\n preco: " +preco);
+        System.out.println("\n total: " +total);
+    }
+
+}
