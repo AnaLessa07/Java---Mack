c9ef51572a9e48aa3259cff8ef714d75b08746db
Ana Lessa <analessaferreira.aa@gmail.com>
2025-09-23 00:26:01 +0000

    JAVAAAA

--- aula_04_teorica/Exer06.java
+++ aula_04_teorica/Exer06.java
@@ -1,29 +1,44 @@
 package aula_04_teorica;
 
+import java.util.Scanner;
+
 public class Exer06 {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner (System.in);
-        int [][] matriz = {
-            {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-            {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-            {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-            {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-            {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-            {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-            {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-            {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-            {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-            {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+        Scanner entrada = new Scanner(System.in);
+        int[][] matriz = {
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11 }
         };
 
-        for (int i = 0; i < matriz.length; i++){
-            for (int j = 0; j < matriz[i].length; j++){
-                System.out.println("Digite um valor:");
-                int num = entrada.nextInt();
+        System.out.println("Digite um valor para comparar:");
+        int num = entrada.nextInt();
 
-                if ()
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+
+                if (matriz[i][j] == num) {
+                    System.out.printf("COLUNA: %d, LINHA: %d%n", j, i);
+                } else {
+                    System.err.println("NAO ENCONTRADO NA MATRIZ");
+                }
+            }
+
+        }
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = i + j;
+                System.out.print(matriz[i][j] + " ");
             }
 
         }
     }
+
 }
