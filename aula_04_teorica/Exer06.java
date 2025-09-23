932ee04353aa0736d9a8e2aa7cbfb0d8b2c4ae0c
Ana Lessa <analessaferreira.aa@gmail.com>
2025-09-23 00:32:55 +0000

    agora foi

--- aula_04_teorica/Exer06.java
+++ aula_04_teorica/Exer06.java
@@ -34,10 +34,9 @@ public class Exer06 {
         }
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
-                matriz[i][j] = i + j;
                 System.out.print(matriz[i][j] + " ");
             }
-
+            System.out.println();
         }
     }
 
